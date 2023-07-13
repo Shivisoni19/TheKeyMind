e54e758f2ae99212327dd17b3b7f229356adaa56
shivani soni <kajalsoni1845@gmail.com>
2023-07-13 20:29:30 +0530

    Ten Commit 13July

--- src/components/Blogs/BlogLeftSide.jsx
+++ src/components/Blogs/BlogLeftSide.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link }from 'react-router-dom'
+import BlogSlider from './BlogSlider'
 
 const BlogLeftSide = () => {
   return (
@@ -27,39 +28,7 @@ const BlogLeftSide = () => {
               </div>
             </div>
 
-            {/* <div className="blog-post mb50">
-              <div className="blog-item">
-                <div className="blog-img">
-                  <div className="flexslider">
-                    <ul className="slides">
-                      <li>
-                        <img alt="" src="img/upload/blog/blog3.jpg" />
-                      </li>
-                      <li>
-                        <img alt="" src="img/upload/blog/blog2.jpg" />
-                      </li>
-                      <li>
-                        <img alt="" src="img/upload/blog/blog3.jpg" />
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="calendar">
-                    <p>02</p>
-                    <span>dec</span>
-                  </div>
-                  <div className="blog-content">
-                    <h1>This is Gallery post with image</h1>
-                    <div className="post-meta">
-                      <Link href="#"><i className="fa fa-user"></i> Joeby Ragpa</Link>
-                      <Link href="#"><i className="fa fa-bookmark"></i> Design</Link>
-                      <Link href="#"><i className="fa fa-comment"></i> 15 Comments</Link>
-                    </div>
-                    <p>We possess within us two minds. So far I have written only of the conscious mind. I would now like to introduce you to your second mind, the hidden and mysterious subconscious. Our subconscious mind contains such power and complexity that it literally staggers the imagination.</p>
-                    <Link href="#" className="small-button button-red mb10">Read More</Link>
-                  </div>
-                </div>
-              </div>
-            </div> */}
+            <BlogSlider/>
 
             <div className="blog-post mb50">
               <div className="blog-item">
@@ -123,17 +92,6 @@ const BlogLeftSide = () => {
                   </div>
                 </div>
               </div>
-            </div>
-
-            <div className="pagenation blog-pagenation clearfix">
-              <ul>
-                <li className="active"><Link href="#">1</Link></li>
-                <li><Link href="#">2</Link></li>
-                <li><Link href="#">3</Link></li>
-                <li><Link href="#">4</Link></li>
-                <li><Link href="#">5</Link></li>
-                <li><Link href="#">&gt;</Link></li>
-              </ul>
             </div>
             <div className="clear"></div>
           </div>
--- src/components/Blogs/BlogSlider.jsx
+++ src/components/Blogs/BlogSlider.jsx
@@ -1,44 +1,131 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './Blog.css'
+
 
 const BlogSlider = () => {
-  return (
-    <div>
-      <div className="blog-post mb50">
-              <div className="blog-item">
-                <div className="blog-img">
-                  <div className="flexslider">
-                    <ul className="slides">
-                      <li>
-                        <img alt="" src="img/upload/blog/blog3.jpg" />
-                      </li>
-                      <li>
-                        <img alt="" src="img/upload/blog/blog2.jpg" />
-                      </li>
-                      <li>
-                        <img alt="" src="img/upload/blog/blog3.jpg" />
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="calendar">
-                    <p>02</p>
-                    <span>dec</span>
-                  </div>
-                  <div className="blog-content">
-                    <h1>This is Gallery post with image</h1>
-                    <div className="post-meta">
-                      <Link href="#"><i className="fa fa-user"></i> Joeby Ragpa</Link>
-                      <Link href="#"><i className="fa fa-bookmark"></i> Design</Link>
-                      <Link href="#"><i className="fa fa-comment"></i> 15 Comments</Link>
-                    </div>
-                    <p>We possess within us two minds. So far I have written only of the conscious mind. I would now like to introduce you to your second mind, the hidden and mysterious subconscious. Our subconscious mind contains such power and complexity that it literally staggers the imagination.</p>
-                    <Link href="#" className="small-button button-red mb10">Read More</Link>
-                  </div>
+    const sliderSettings = {
+      dots: true,
+      arrow:true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 2000,
+    };
+  
+    return (
+      <div>
+        <Slider {...sliderSettings}>
+          <div className="blog-post mb50">
+            <div className="blog-item">
+              <div className="blog-img">
+                <img alt="" src="img/blog/latest-guide-1.jpg" />
+              </div>
+              <div className="calendar">
+                <p>02</p>
+                <span>dec</span>
+              </div>
+              <div className="blog-content">
+                <h1>This is Gallery post with image</h1>
+                <div className="post-meta">
+                  <Link href="#">
+                    <i className="fa fa-user"></i> Joeby Ragpa
+                  </Link>
+                  <Link href="#">
+                    <i className="fa fa-bookmark"></i> Design
+                  </Link>
+                  <Link href="#">
+                    <i className="fa fa-comment"></i> 15 Comments
+                  </Link>
                 </div>
+                <p>
+                  We possess within us two minds. So far I have written only of the conscious mind. I would now like to
+                  introduce you to your second mind, the hidden and mysterious subconscious. Our subconscious mind
+                  contains such power and complexity that it literally staggers the imagination.
+                </p>
+                <Link href="#" className="small-button button-red mb10">
+                  Read More
+                </Link>
               </div>
             </div>
-    </div>
-  )
-}
-
-export default BlogSlider
+          </div>
+  
+          <div className="blog-post mb50">
+            <div className="blog-item">
+              <div className="blog-img">
+                <img alt="" src="img/blog/latest-guide-2.jpg" />
+              </div>
+              <div className="calendar">
+                <p>02</p>
+                <span>dec</span>
+              </div>
+              <div className="blog-content">
+                <h1>This is Gallery post with image</h1>
+                <div className="post-meta">
+                  <Link href="#">
+                    <i className="fa fa-user"></i> Joeby Ragpa
+                  </Link>
+                  <Link href="#">
+                    <i className="fa fa-bookmark"></i> Design
+                  </Link>
+                  <Link href="#">
+                    <i className="fa fa-comment"></i> 15 Comments
+                  </Link>
+                </div>
+                <p>
+                  We possess within us two minds. So far I have written only of the conscious mind. I would now like to
+                  introduce you to your second mind, the hidden and mysterious subconscious. Our subconscious mind
+                  contains such power and complexity that it literally staggers the imagination.
+                </p>
+                <Link href="#" className="small-button button-red mb10">
+                  Read More
+                </Link>
+              </div>
+            </div>
+          </div>
+  
+          <div className="blog-post mb50">
+            <div className="blog-item">
+              <div className="blog-img">
+                <img alt="" src="img/blog/latest-guide-3.jpg" />
+              </div>
+              <div className="calendar">
+                <p>02</p>
+                <span>dec</span>
+              </div>
+              <div className="blog-content">
+                <h1>This is Gallery post with image</h1>
+                <div className="post-meta">
+                  <Link href="#">
+                    <i className="fa fa-user"></i> Joeby Ragpa
+                  </Link>
+                  <Link href="#">
+                    <i className="fa fa-bookmark"></i> Design
+                  </Link>
+                  <Link href="#">
+                    <i className="fa fa-comment"></i> 15 Comments
+                  </Link>
+                </div>
+                <p>
+                  We possess within us two minds. So far I have written only of the conscious mind. I would now like to
+                  introduce you to your second mind, the hidden and mysterious subconscious. Our subconscious mind
+                  contains such power and complexity that it literally staggers the imagination.
+                </p>
+                <Link href="#" className="small-button button-red mb10">
+                  Read More
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Slider>
+      </div>
+    );
+  };
+  
+  export default BlogSlider;
+  
